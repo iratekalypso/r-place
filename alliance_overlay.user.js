e85678859884d7558d7d51099500b2de545788cd
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-01 15:46:33 -0400

    Update alliance_overlay.user.js

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/iratekalypso/r-place-2022/blob/main/Overlay.png";
+            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959536194490884136/Overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
