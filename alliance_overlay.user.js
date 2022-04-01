498a202a414bb81a67cbd83ae1b2e4a76f60c466
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-01 15:49:04 -0400

    Update alliance_overlay.user.js

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -6,8 +6,8 @@
 // @author       /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/master/alliance_overlay.user.js
-// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/master/alliance_overlay.user.js
+// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
+// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
