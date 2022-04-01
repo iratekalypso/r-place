239d1004b4391268d2faf8c5945af54e0cf13a57
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-01 18:35:45 -0400

    Update alliance_overlay.user.js

--- alliance_overlay.user.js
+++ alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         April Fools
 // @namespace    http://tampermonkey.net/
-// @version      0.85
+// @version      0.9
 // @description  Keep the canvas beautiful!
 // @author       /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
