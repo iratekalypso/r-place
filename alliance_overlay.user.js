63d3a76a4c9482e464d9201cf99cd3df07067022
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-01 15:40:29 -0400

    beta
    overlay script

---
+++ alliance_overlay.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         April Fools
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  Keep the canvas beautiful!
+// @author       /u/irate_kalypso
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateurl    https://github.com/iratekalypso/r-place-2022/raw/master/alliance_overlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959536194490884136/Overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
