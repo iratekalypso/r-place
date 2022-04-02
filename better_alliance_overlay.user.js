bcdb1a8563f25ded4286b0ce289c484b5bec5840
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 06:29:44 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959761243319721984/overlay_better_version_v4.5.png";
+            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959761513755860992/overlay_better_version_v4.6.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
