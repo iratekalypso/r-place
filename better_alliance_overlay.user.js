27e94557ce8564168d9429727cd4dd637706d323
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 01:40:44 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         April Fools
+// @name         April Fools r/starwars_place Allies
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      0.1
 // @description  Keep the canvas beautiful!
 // @author       /u/irate_kalypso
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
-// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/alliance_overlay.user.js
+// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
+// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959681569105248306/overlay_v3.8.png";
+            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959688389584650241/overlay_better_version.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
