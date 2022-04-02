f505173ab84909c0adee59c07a70b76f49a7c125
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 14:14:41 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,26 +1,27 @@
 // ==UserScript==
-// @name         April Fools r/starwars_place Allies
+// @name         osu! Logo template
 // @namespace    http://tampermonkey.net/
-// @version      3.5
-// @description  Keep the canvas beautiful!
-// @author       oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
+// @version      0.2
+// @description  try to take over the canvas!
+// @author       oralekin, LittleEndu
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
-// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://sot.spacescience.tech/static/overlay.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-            console.log(i);
-            return i;
-        })())
-
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = "https://sot.spacescience.tech/static/overlay.png";
+                i.onload = () => {
+                    if (i.width === i.height) {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                    } else {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+                    }
+                };
+                return i;
+            })())
     }, false);
-
 }
