431c28fec1f48f41ab07443c3338e8635c9c9e29
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 09:02:30 -0400

    Updated "Raphael School of Athens"

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         April Fools r/starwars_place Allies
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.5
 // @description  Keep the canvas beautiful!
 // @author       oralekin from osu! /r/osuplace
+// @overlay maintainer /u/irate_kalypso /r/starwars_place
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
@@ -15,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/958580455777644576/959784360943443998/overlay_better_version_v5.2_forked_for_hk_with_petal_fix_and_color_tuning.png";
+            i.src = "https://cdn.discordapp.com/attachments/708198120617672734/959799536929349632/overlay_better_version_v6_school_of_athens_update.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
