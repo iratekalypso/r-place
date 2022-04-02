20c7208e139f60b08b41fcb3f657a8c05f73724b
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 05:36:43 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/iratekalypso/r-place-2022/blob/main/overlay_alliance_better_version.png";
+            i.src = "https://github.com/iratekalypso/r-place-2022/blob/c3f3e8b3d87b5d3b33c85db28daed16b318b5210/overlay_alliance_better_version.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
