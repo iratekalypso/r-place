3cd8de2ce914d69e83f1f6b21de8166fa7dae510
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 07:30:58 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         April Fools r/starwars_place Allies
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Keep the canvas beautiful!
-// @author       /u/irate_kalypso
+// @author       oralekin from osu! /r/osuplace
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
@@ -15,7 +15,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959768818425683999/overlay_better_version_v5.png";
+            i.src = "https://cdn.discordapp.com/attachments/724673162842734662/959776902216839239/overlay_better_version_v5.1.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
