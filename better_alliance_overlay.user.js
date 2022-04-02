f5438f3fda955a5c0273206b8047daf3b9dc89bd
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 13:39:01 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         April Fools r/starwars_place Allies
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.5
 // @description  Keep the canvas beautiful!
 // @author       oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
 // @match        https://hot-potato.reddit.com/embed*
