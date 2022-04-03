c7e74a0507ca465c8f3a30abb6f87b8bf29e26b7
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-02 20:45:13 -0400

    update AprilKnights imagery!

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/959905843556520039/959971714547187742/newCanvas.png";
+                i.src = "https://cdn.discordapp.com/attachments/959905843556520039/959976429414658128/newCanvas.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
