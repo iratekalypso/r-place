adee790eeada8118bc54640d2ae1dfff40911f4c
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2022-04-03 02:23:39 -0400

    Update better_alliance_overlay.user.js

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -1,27 +1,36 @@
 // ==UserScript==
-// @name         April Fools r/starwars_place Allies
+// @name         osu! Logo template
 // @namespace    http://tampermonkey.net/
-// @version      8.0
-// @description  Keep the canvas beautiful!
-// @author       /r/AprilKnights, oralekin from osu! /r/osuplace, maintainer /u/irate_kalypso /r/starwars_place
+// @version      0.3
+// @description  try to take over the canvas!
+// @author       oralekin, LittleEndu, ekgame
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/959905843556520039/959976429414658128/newCanvas.png";
-                i.onload = () => {
-                    if (i.width === i.height) {
-                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                    } else {
-                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
-                    }
-                };
-                return i;
-            })())
+        // Load the image
+        const image = document.createElement("img");
+        image.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+        image.onload = () => {
+            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+        };
+      
+        // Add the image as overlay
+        const camera = document.querySelector("mona-lisa-embed").shadowRoot.querySelector("mona-lisa-camera");
+        const canvas = camera.querySelector("mona-lisa-canvas");
+        canvas.shadowRoot.querySelector('.container').appendChild(image);
+      
+        // Add a style to put a hole in the pixel preview (to see the current or desired color)
+        const waitForPreview = setInterval(() => {
+            const preview = camera.querySelector("mona-lisa-pixel-preview");
+            if (preview) {
+              clearInterval(waitForPreview);
+              const style = document.createElement('style')
+              style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
+              preview.shadowRoot.appendChild(style);
+            }
+        }, 100);
     }, false);
 }
