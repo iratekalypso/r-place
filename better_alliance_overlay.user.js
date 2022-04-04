0678f1854a051889f29aeea93647977184f61819
Eduard Tanase <eduard.tanase1@gmail.com>
2022-04-03 20:03:08 -0700

    Fix pixel offset

--- better_alliance_overlay.user.js
+++ better_alliance_overlay.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
         const image = document.createElement("img");
         image.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
         image.onload = () => {
-            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
+            image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1; margin-left: 1px; margin-top: 1px`;
         };
       
         // Add the image as overlay
